bcb8c5bcec7ce7483e100feee6937e454288b019
Landry GAIFFE <landry.gaiffe@gmail.com>
2024-03-02 15:38:25 +0100

    willow: partiel revert: Move to IFAAService in hardware/xiaomi

--- willow-vendor.mk
+++ willow-vendor.mk
@@ -3594,6 +3594,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/willow/proprietary/vendor/radio/qcril_database/upgrade/9_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/9_version_update_ecc_table.sql
 
 PRODUCT_PACKAGES += \
+    IFAAService \
     SoterService \
     CACertService \
     CneApp \
